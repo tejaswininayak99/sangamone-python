5ed008384b8064fe775a8ed241a21a9640c53f2a
tejaswininayak99 <98076959+tejaswininayak99@users.noreply.github.com>
2022-03-15 21:29:20 +0530

    Add files via upload

---
+++ Assignment5/11e(assignment).py
@@ -0,0 +1,37 @@
+def diamond2(str):
+    
+    s1=str
+    s2=" "
+    len1=len(s1)
+    
+   
+    for j in range(1,len1+1,1):   
+        for i in range(0,len1-j*(-1),1):
+            print(s2,end="")
+        print(s1[0:j])
+
+    for j in range(1,len1*(+1),1):   
+        for i in range(0,j,1):
+            print(s2,end="")
+        print(s1[0:len1-j])
+   
+'''
+    for j in range(1,len1+1,1):   
+        for i in range(0,len1-j,1):
+            print(s2,end="")
+        print(s1[0:j])
+    for j in range(1,len1+1,1):   
+        for i in range(0,len1+j,1):
+            print(s2,end="")
+        print(s1[0:j])
+
+    for j in range(1,len1,1):   
+        for i in range(0,j,1):
+            print(s2,end="")
+        print(s1[0:len1-j])
+    for j in range(1,len1-1,1):   
+        for i in range(0,len1-j,1):
+            print(s2,end="")
+        print(s1[0:len1-j])
+   '''     
+diamond2("TejaswiniNayak")
