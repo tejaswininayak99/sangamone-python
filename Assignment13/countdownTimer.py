c377bfe69724d6852988c49bdc45991f79b1f5bb
tejaswininayak99 <98076959+tejaswininayak99@users.noreply.github.com>
2022-03-30 21:07:33 +0530

    Add files via upload

---
+++ Assignment13/countdownTimer.py
@@ -0,0 +1,19 @@
+import time
+import datetime
+
+def timer(m,s):
+    t_sec = m * 60 + s
+
+    while t_sec > 0:
+    #to analyize when the timer reaches 0
+        t = datetime.timedelta(seconds = t_sec)
+        #timedelta tells the diff btwn two time
+        print(t, end="\n")
+
+        time.sleep(1)
+        #sleeps delays the program
+        t_sec -= 1
+        
+m = input("Enter the minutes:\n")
+s = input("Enter the seconds:\n")
+timer(int(m), int(s))
